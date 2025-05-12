0561f9aa97f267f11ca54324ee5414c8cb46caed
vysakhviswama <vysakhma@google.com>
2025-05-12 08:17:00 +0000

    Conflict resolved

--- src/login/LoginInterfaces.ts
+++ src/login/LoginInterfaces.ts
@@ -13,7 +13,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  */
 
 export interface IAuthCredentials {
@@ -23,7 +23,6 @@ export interface IAuthCredentials {
   config_error?: number;
   login_error?: number;
 }
-
 export interface LoginDialogProps {
   loginError: boolean;
   configError: boolean;
