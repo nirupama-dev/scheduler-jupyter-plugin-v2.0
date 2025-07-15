a2ec6009f6d9422073d9f3db49769d436e319b0f
risha-vijayvargiya-22 <risha.vijayvargiya@accenture.com>
2025-07-15 04:40:04 +0000

    added services folder and its dependencies in new src

---
+++ src/utils/Constants.ts
@@ -0,0 +1,37 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { ConfigService } from '../services/common/ConfigService';
+
+export const LOGIN_STATE = '1';
+export const STATUS_SUCCESS = 'SUCCEEDED';
+export const API_HEADER_BEARER = 'Bearer ';
+export const API_HEADER_CONTENT_TYPE = 'application/json';
+export type scheduleMode = 'runNow' | 'runSchedule';
+export const gcpServiceUrls = (async () => {
+  return await ConfigService.gcpServiceUrlsAPI();
+})();
+export const ABORT_MESSAGE = 'signal is aborted without reason';
+export const HTTP_STATUS_BAD_REQUEST = 400;
+export const HTTP_STATUS_FORBIDDEN = 403;
+
+// Pattern to check whether string contains link
+export const pattern =
+  // eslint-disable-next-line
+  /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g; // REGX to extract URL from string
+
+export const DEFAULT_TIME_ZONE = 'UTC';
