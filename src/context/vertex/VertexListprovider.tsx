1623fd42d97b223db6ecaa52d4992ca597417291
vysakhviswama <vysakhma@google.com>
2025-09-02 15:43:59 +0000

    Restore pagination implemented

---
+++ src/context/vertex/VertexListprovider.tsx
@@ -0,0 +1,35 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React, { ReactNode, useState } from 'react';
+import { IActivePaginationVariables } from '../../interfaces/VertexInterface';
+import { VertexListContext } from './VertexListContext';
+
+// Provider component
+export const VertexListProvider = ({ children }: { children: ReactNode }) => {
+  const [activePaginationVariables, setActivePaginationVariables] =
+    useState<IActivePaginationVariables | null>(null);
+  const value = {
+    activePaginationVariables,
+    setActivePaginationVariables
+  };
+  return (
+    <VertexListContext.Provider value={value}>
+      {children}
+    </VertexListContext.Provider>
+  );
+};
