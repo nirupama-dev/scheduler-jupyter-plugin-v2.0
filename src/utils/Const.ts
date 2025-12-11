3ce00f8d4b3d2be7eca608ed0c86f66beceb4d7c
vysakhviswama <vysakhma@google.com>
2025-12-11 12:02:17 +0000

    Region values updated in vertex create form

--- src/utils/Const.ts
+++ src/utils/Const.ts
@@ -98,18 +98,38 @@ export const DEFAULT_MACHINE_TYPE = 'n1-standard-2 (2 CPUs, 8.05 GB RAM)';
 export const DEFAULT_KERNEL = 'python3';
 
 export const VERTEX_REGIONS = [
+  'africa-south1',
+  'northamerica-northeast1',
+  'southamerica-east1',
+  'us-central1',
+  'us-east1',
+  'us-east4',
+  'us-east5',
+  'us-south1',
+  'us-west1',
+  'us-west2',
+  'us-west4',
+  'asia-east1',
   'asia-east2',
   'asia-northeast1',
   'asia-northeast3',
   'asia-south1',
   'asia-southeast1',
+  'asia-southeast2',
   'australia-southeast1',
+  'europe-central2',
+  'europe-north1',
+  'europe-southwest1',
   'europe-west1',
-  'northamerica-northeast1',
-  'southamerica-east1',
-  'us-central1',
-  'us-west1',
-  'us-west4'
+  'europe-west2',
+  'europe-west3',
+  'europe-west4',
+  'europe-west6',
+  'europe-west8',
+  'europe-west12',
+  'me-central1',
+  'me-central2',
+  'me-west1'
 ];
 
 export const DEFAULT_SERVICE_ACCOUNT = 'compute@developer.gserviceaccount.com';
